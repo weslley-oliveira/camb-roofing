fa85311b228f034cc4289237ec296f84108e3b17
bobzera <weslleyzera2020@gmail.com>
2021-03-27 20:35:14 +0000

    meta

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,16 @@ export default function Home() {
       <Head>
         <title>Camb Roofing</title>
         <link rel="icon" href="/favicon.ico"/>
+        <meta
+          name="description"
+          content="Our website is currently undergoing scheduled maintenance. We should be working shortly. Thank you for your patience."
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/Camb.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-bw-logo.svg&widths=50&heights=50`}
+        />
+        <meta name="og:title" content="Camb Roofing" />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
       <main className="flex flex-col  justify-center flex-1 px-4 sm:px-20 text-gray-700">
